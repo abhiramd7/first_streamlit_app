7ded390f1f1ca37f09980fdd3abdf8009863a86c
abhiramd7 <155402391+abhiramd7@users.noreply.github.com>
2024-02-02 22:22:25 -0500

    Update streamlit_app.py
    Allow user to select from fruit list added mulitselect future.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,9 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(fruit_list)
