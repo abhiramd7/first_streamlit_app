5c2ce1151dfd95be0203237a42bc7a6894f12ca5
abhiramd7 <155402391+abhiramd7@users.noreply.github.com>
2024-02-10 21:36:49 -0500

    Update streamlit_app.py
    Add another fruity to menu in normalized response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalize json responses
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the normalized respone on App
+streamlit.dataframe(fruityvice_normalized)
