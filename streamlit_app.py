5674c5a80aa3301ab643d36a9d68a0d380210e6e
abhiramd7 <155402391+abhiramd7@users.noreply.github.com>
2024-02-10 21:38:01 -0500

    Update streamlit_app.py
    bug, correct the pandas calling function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 # Normalize json responses
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Display the normalized respone on App
 streamlit.dataframe(fruityvice_normalized)
