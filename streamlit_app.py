52c01e2677af8659c6c8fbf9a13de9d8e1c3221a
abhiramd7 <155402391+abhiramd7@users.noreply.github.com>
2024-02-25 19:43:02 -0500

    fix the hardcode value for snowflake insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if streamlit.button('Get Fruit Load List'):
 # Allow enduser to add the fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+    my_cur.execute("insert into FRUIT_LOAD_LIST values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
@@ -65,7 +65,5 @@ if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   insert_return_message = insert_row_snowflake(add_my_fruit)
   streamlit.text(insert_return_message)
-
-
 
 streamlit.stop()
